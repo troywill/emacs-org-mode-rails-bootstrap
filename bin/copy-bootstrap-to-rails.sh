99536615c1dbbc8f3baabc2e71508326d0675d61
Troy Will <troydwill@gmail.com>
2013-05-13 19:02:42 -0700

    minified version

--- bin/copy-bootstrap-to-rails.sh
+++ bin/copy-bootstrap-to-rails.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # ./bin/copy-bootstrap-to-rails.sh
-cp -iv bootstrap/css/bootstrap.min.css ../app/assets/stylesheets
-cp -iv bootstrap/css/bootstrap-responsive.css ../app/assets/stylesheets
-cp -iv bootstrap/js/bootstrap.min.js ../app/assets/javascripts
+cp -iv bootstrap/css/bootstrap.min.css ../app/assets/stylesheets/
+cp -iv bootstrap/css/bootstrap-responsive.min.css ../app/assets/stylesheets/
+cp -iv bootstrap/js/bootstrap.min.js ../app/assets/javascripts/
